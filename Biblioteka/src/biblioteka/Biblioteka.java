e6d3f32cf3bccf4a330eed3fc1385ecca78d6e06
anaslovic <slovicana0@gmail.com>
2018-03-09 14:45:48 +0100

    Uvedena logicka kontrola u klasu Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -8,24 +8,30 @@ public class Biblioteka implements BibliotekaInterfejs {
 	private LinkedList <Knjiga> knjige = new LinkedList <Knjiga>();
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga==null || knjige.contains(knjiga))
+			throw new RuntimeException ("Knjiga je null ili vec postoji u biblioteci.");
+		
 		knjige.add(knjiga);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(knjiga==null || !knjige.contains(knjiga))
+			throw new RuntimeException ("Knjiga je null ili ne postoji u biblioteci.");
 		knjige.remove(knjiga);
 
 	}
 
 	@Override
 	public LinkedList<Knjiga> vratiSveKnjige() {
-		// TODO Auto-generated method stub
 		return knjige;
 	}
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
+		if(naslov==null)
+			throw new RuntimeException("Morate uneti naslov.");
 		LinkedList <Knjiga> novaLista = new LinkedList<Knjiga>();
 		
 		for(int i=0;i<knjige.size();i++)
